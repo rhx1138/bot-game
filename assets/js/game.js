ccd562f8abd8dbb1c127872638000d96d47d5bd6
rhx1138 <ryanjhendrix@gmail.com>
2022-06-17 14:51:38 -0400

    added more functionality to player / enemy attack

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,12 @@
 let playerName = window.prompt('Please enter your name');
 let playerHealth = 100;
 let playerAttack = 10;
+let playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
+console.log(playerName, playerAttack, playerHealth, playerMoney);
 
 let enemyName = "Roboman";
+let enemyNames = ["Zem", "Noak", "Demak"];
 let enemyHealth = 50;
 let enemyAttack = 12;
 
@@ -21,7 +23,7 @@ let fight = () => {
 
         enemyHealth = enemyHealth - playerAttack;
         
-        console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health remaining.`);
+        console.log(`${playerName} attacked ${enemyNames}. ${enemyNames} now has ${enemyHealth} health remaining.`);
 
         // check enemy health
         if (enemyHealth <= 0) {
@@ -34,7 +36,7 @@ let fight = () => {
 
         playerHealth = playerHealth - enemyAttack;
         
-        console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining.`);
+        console.log(`${enemyNames} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining.`);
 
         // check player health
         if (playerHealth <= 0) {
@@ -44,10 +46,31 @@ let fight = () => {
         }
 
     } else if (promptFight === "skip" || promptFight === "SKIP") {
-        window.alert(playerName + " has chosen to skip");
+        // confirm player wants to skip
+        let confirmSkip = window.confirm("are you sure you want to skip?");
+
+        // if yes, leave fight
+        if (confirmSkip) {
+            window.alert(`${playerName} will skip this fight`);
+
+            //subtract money for skipping turn
+            playerMoney = playerMoney -2;
+
+            window.alert(`You have ${playerMoney} left`)
+            console.log(`${playerName} has $${playerMoney} left`);
+        }
+
     } else {
-        window.alert('You need to choose an option');
+        for(var i = 0; i < enemyNames.length; i++) {
+            debugger;
+            fight(enemyNames[i]);
+        }
+        
     }
 };
 
 fight();
+
+// for(var i = 0; i < enemyNames.length; i++) {
+//     fight(enemyNames[i]);
+//     }
