a3fe6a7dceae3f2f7f7f414412888ed3abd50ac9
rhx1138 <ryanjhendrix@gmail.com>
2022-06-17 11:22:59 -0400

    added player attack and enemy attack rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,4 @@
-// let playerName = window.prompt('Please enter your name');
-let playerName = "Jam";
+let playerName = window.prompt('Please enter your name');
 let playerHealth = 100;
 let playerAttack = 10;
 
@@ -9,18 +8,46 @@ let enemyName = "Roboman";
 let enemyHealth = 50;
 let enemyAttack = 12;
 
+enemyHealth = enemyHealth - playerAttack;
+playerHealth = playerHealth - enemyAttack;
+
+
 let fight = () => {
-    // window.alert("welcome to thunderdome");
-    enemyHealth = enemyHealth - playerAttack;
-    playerHealth = playerHealth - enemyAttack;
+    window.alert("welcome to thunderdome");
+
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+
+        enemyHealth = enemyHealth - playerAttack;
+        
+        console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health remaining.`);
+
+        // check enemy health
+        if (enemyHealth <= 0) {
+            window.alert(`${enemyName} has been defeated`);
+        } else {
+            window.alert(`${enemyName} still has ${enemyHealth} health left.`);
+        }
+
+
+
+        playerHealth = playerHealth - enemyAttack;
+        
+        console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining.`);
+
+        // check player health
+        if (playerHealth <= 0) {
+            window.alert(`${playerName} has been defeated`);
+        } else {
+            window.alert(`${playerName} still has ${playerHealth} health left.`);
+        }
+
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
+        window.alert(playerName + " has chosen to skip");
+    } else {
+        window.alert('You need to choose an option');
+    }
 };
 
 fight();
-
-
-
-console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health remaining.`);
-console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining.`);
-
-
-
